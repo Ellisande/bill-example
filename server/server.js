dff7ed4d6f7b2812a3903cb12afd0c3fce9d78a3
Ellisande <justin.dragos@gmail.com>
2014-10-11 11:40:32 -0700

    Inserted some test data into the Bills collection.

--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
 Bills = new Meteor.Collection('bills');
+
+// Bills.insert({name: 'T-Mobile', amount: 97.09, dueDate: moment('2014-10-21').toDate(), website:'http://tmobile.com', paid: false, frequency: 'month'});
+// Bills.insert({name: 'Cox', amount: 150.00, dueDate: moment('2014-10-14').toDate(), website:'http://cox.com', paid: false, frequency: 'month'});
+// Bills.insert({name: 'Ford', amount: 350.00, dueDate: moment('2014-10-19').toDate(), website:'https://credit.ford.com/accountmanager/amo/fcus/US/en', paid: false, frequency: 'month'});
+// Bills.insert({name: 'Mesa', amount: 79.53, dueDate: moment('2014-10-25').toDate(), website:'https://cis.mesaaz.gov/eservices/p_template', paid: false, frequency: 'month'});
+// Bills.insert({name: 'SRP', amount: 327.33, dueDate: moment('2014-10-29').toDate(), website:'https://myaccount.srpnet.com/SSO/Dashboard', paid: false, frequency: 'month'});
+// Bills.insert({name: 'Mortgage', amount: 1400, dueDate: moment('2014-10-16').toDate(), website:'https://credit.ford.com/accountmanager/amo/fcus/US/en', paid: false, frequency: 'month'});
+// Bills.insert({name: 'Insurance', amount: 298.00, dueDate: moment('2014-10-16').toDate(), website:'https://www.statefarm.com', paid: false, frequency: 'month'});
+// Bills.insert({name: 'Liam Daycare', amount: 200.00, dueDate: moment('2014-10-28').toDate(), website:'https://www.statefarm.com', paid: false, frequency: 'week'});
+// Bills.insert({name: 'Dog Daycare', amount: 120.00, dueDate: moment('2014-10-28').toDate(), website:'http://barkavepet.com/', paid: false, frequency: 'week'});
+// Bills.insert({name: 'Amex', amount: 200.00, dueDate: moment('2014-10-21').toDate(), website:'https://www.americanexpress.com/', paid: false, frequency: 'month'});
