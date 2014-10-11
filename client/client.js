4e76677bc8ff36e67b0755de82168b932a039f5f
Ellisande <justin.dragos@gmail.com>
2014-10-11 11:55:57 -0700

    Cleaned out the references to the "hello" template from client.js

--- client/client.js
+++ client/client.js
@@ -1,16 +1,1 @@
 Bills = new Meteor.Collection('bills');
-// counter starts at 0
-Session.setDefault("counter", 0);
-
-Template.hello.helpers({
-  counter: function () {
-    return Session.get("counter");
-  }
-});
-
-Template.hello.events({
-  'click button': function () {
-    // increment the counter when button is clicked
-    Session.set("counter", Session.get("counter") + 1);
-  }
-});
